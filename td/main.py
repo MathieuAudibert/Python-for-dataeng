29d80f3e53d98c415e7f74fbbd2259215ea2fe20
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-10-08 14:07:51 +0200

    exo 2 td

--- td/main.py
+++ td/main.py
@@ -1,7 +1,23 @@
+import math
+
 def sort_distinct_words():
     phrase = input("Phrase: ")
     words = phrase.split()
     sorted_words = sorted(words, key=lambda w: (len(w), w))
     print(sorted_words)
-sort_distinct_words()
+#sort_distinct_words()
 
+def clean_list(L):
+    for x in L[:]:
+        if not isinstance(x, int) and not isinstance(x, str) and not isinstance(x, float):
+            L.remove(x)
+        elif isinstance(x, str):
+            if not x.isdigit():
+                L.remove(x)
+            else:
+                L[L.index(x)] = int(x)
+        elif isinstance(x, float):
+            L[L.index(x)] = math.floor(x)
+    print(L) 
+L = [12, "15", None, "abc", 8, "20", 3.5, "NaN"]
+#clean_list(L)
