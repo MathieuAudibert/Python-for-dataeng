257f9605196f2bd008f05d3f72947f6c087c8353
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-10-08 18:53:53 +0200

    📃 docs: readability

--- td/exo7.py
+++ td/exo7.py
@@ -27,12 +27,13 @@ def moyenne_gen(liste_student):
     print(liste_student)
     class_moy = []
     for i in liste_student:
+        student_moy = {}
         notes = i.get("notes")
+        
         if isinstance(notes, list):
             for x in notes[:]:
                 notes[notes.index(x)] = int(x)
         
-        student_moy =  {}
         student_moy["nom"] = i.get("nom")
         student_moy["moyenne"] = sum(notes) / len(i.get("notes"))
         class_moy.append(student_moy)
@@ -49,7 +50,7 @@ def main():
     add_student(liste_student)
     remove_student(liste_student)
     moyenne_gen(liste_student)
-    #student_best_moy(liste_student)
+    student_best_moy(liste_student)
 
 if __name__ == "__main__":
     main()
