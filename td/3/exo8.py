caa37b8d1ba823806241d97904b3d234c2838500
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-10-15 15:58:50 +0200

    ✨ feat: exo8

---
+++ td/3/exo8.py
@@ -0,0 +1,14 @@
+class Vehicule:
+    def __init__(self, vitesse_max, kilometrage):
+        self.vitesse_max = vitesse_max
+        self.kilometrage = kilometrage
+
+class Bus(Vehicule):
+    def __init__(self, vitesse_max, kilometrage):
+        super().__init__(vitesse_max, kilometrage)
+
+    def __str__(self):
+        return f"Vitesse max: {self.vitesse_max}, Kilometrage: {self.kilometrage}"
+
+bus = Bus(120, 120000)
+print(bus)
